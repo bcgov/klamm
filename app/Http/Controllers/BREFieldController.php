57e11f8d23f8f5697f17d28c58c464ccc6d883ad
brysonjbest <103070659+brysonjbest@users.noreply.github.com>
2024-08-28 11:38:03 -0700

    Create custom queries for bre api endpoints.

--- app/Http/Controllers/BREFieldController.php
+++ app/Http/Controllers/BREFieldController.php
@@ -9,9 +9,131 @@ use Illuminate\Support\Facades\Validator;
 
 class BREFieldController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return BREFieldResource::collection(BREField::all());
+        $query = BREField::query();
+
+        if ($request->has('name')) {
+            $query->whereRaw('LOWER(name) LIKE ?', ['%' . strtolower($request->name) . '%']);
+        }
+        if ($request->has('data_type_id')) {
+            $query->where('data_type_id', $request->data_type_id);
+        }
+
+        if ($request->has('label')) {
+            $query->whereRaw('LOWER(label) LIKE ?', ['%' . strtolower($request->label) . '%']);
+        }
+
+        if ($request->has('description')) {
+            $query->whereRaw('LOWER(description) LIKE ?', ['%' . strtolower($request->description) . '%']);
+        }
+
+        if ($request->has('help_text')) {
+            $query->whereRaw('LOWER(help_text) LIKE ?', ['%' . strtolower($request->help_text) . '%']);
+        }
+
+        if ($request->has('field_group')) {
+            $query->whereHas('breFieldGroups', function ($query) use ($request) {
+                $query->whereRaw('LOWER(name) LIKE ?', ['%' . strtolower($request->field_group) . '%']);
+            });
+        }
+
+        if ($request->has('rule_inputs')) {
+            $query->whereHas('breInputs', function ($query) use ($request) {
+                if ($request->filled('rule_inputs')) {
+                    $input = $request->rule_inputs;
+                    if (is_numeric($input)) {
+                        $query->where('bre_rules.id', $input);
+                    } else {
+                        $query->whereRaw('LOWER(bre_rules.name) LIKE ?', ['%' . strtolower($input) . '%'])
+                            ->orWhereRaw('LOWER(bre_rules.label) LIKE ?', ['%' . strtolower($input) . '%'])
+                            ->orWhereRaw('LOWER(bre_rules.description) LIKE ?', ['%' . strtolower($input) . '%']);
+                    }
+                }
+            });
+        }
+
+        if ($request->has('rule_outputs')) {
+            $query->whereHas('breOutputs');
+        }
+
+        if ($request->has('rule_outputs')) {
+            $query->whereHas('breOutputs', function ($query) use ($request) {
+                if ($request->filled('rule_outputs')) {
+                    $input = $request->rule_outputs;
+                    if (is_numeric($input)) {
+                        $query->where('bre_rules.id', $input);
+                    } else {
+                        $query->whereRaw('LOWER(bre_rules.name) LIKE ?', ['%' . strtolower($input) . '%'])
+                            ->orWhereRaw('LOWER(bre_rules.label) LIKE ?', ['%' . strtolower($input) . '%'])
+                            ->orWhereRaw('LOWER(bre_rules.description) LIKE ?', ['%' . strtolower($input) . '%']);
+                    }
+                }
+            });
+        }
+
+        if ($request->has('icmcdw_fields')) {
+            $query->whereHas('icmcdwFields', function ($query) use ($request) {
+                if ($request->filled('icmcdw_fields')) {
+                    $input = $request->icmcdw_fields;
+                    if (is_numeric($input)) {
+                        $query->where('icm_cdw_fields.id', $input);
+                    } else {
+                        $query->whereRaw('LOWER(icm_cdw_fields.name) LIKE ?', ['%' . strtolower($input) . '%'])
+                            ->orWhereRaw('LOWER(icm_cdw_fields.field) LIKE ?', ['%' . strtolower($input) . '%'])
+                            ->orWhereRaw('LOWER(icm_cdw_fields.panel_type) LIKE ?', ['%' . strtolower($input) . '%'])
+                            ->orWhereRaw('LOWER(icm_cdw_fields.entity) LIKE ?', ['%' . strtolower($input) . '%'])
+                            ->orWhereRaw('LOWER(icm_cdw_fields.path) LIKE ?', ['%' . strtolower($input) . '%'])
+                            ->orWhereRaw('LOWER(icm_cdw_fields.subject_area) LIKE ?', ['%' . strtolower($input) . '%'])
+                            ->orWhereRaw('LOWER(icm_cdw_fields.applet) LIKE ?', ['%' . strtolower($input) . '%']);
+                    }
+                }
+            });
+        }
+
+
+
+        if ($request->has('input_output_type')) {
+            $inputOutputType = strtolower($request->input_output_type);
+
+            $query->where(function ($query) use ($inputOutputType) {
+                if ($inputOutputType === 'input') {
+                    $query->whereHas('breInputs');
+                } elseif ($inputOutputType === 'output') {
+                    $query->whereHas('breOutputs');
+                } else {
+                    $query->whereHas('breInputs')
+                        ->orWhereHas('breOutputs');
+                }
+            });
+        }
+
+
+        if ($request->has('order_by')) {
+            $orderDirection = $request->input('order_direction', 'asc');
+            $query->orderBy($request->order_by, $orderDirection);
+        }
+
+        if ($request->has('limit')) {
+            $query->limit($request->limit);
+        }
+
+        if ($request->has('offset')) {
+            $query->offset($request->offset);
+        }
+
+        if ($request->has('search')) {
+            $searchTerm = strtolower($request->search);
+
+            $query->where(function ($query) use ($searchTerm) {
+                $query->whereRaw('LOWER(name) LIKE ?', ['%' . $searchTerm . '%'])
+                    ->orWhereRaw('LOWER(label) LIKE ?', ['%' . $searchTerm . '%'])
+                    ->orWhereRaw('LOWER(description) LIKE ?', ['%' . $searchTerm . '%']);
+            });
+        }
+
+        $breFields = $query->get();
+        return BREFieldResource::collection($breFields);
     }
 
     public function show($id)
--- app/Http/Controllers/ICMCDWFieldController.php
+++ app/Http/Controllers/ICMCDWFieldController.php
@@ -9,9 +9,70 @@ use Illuminate\Support\Facades\Validator;
 
 class ICMCDWFieldController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return ICMCDWFieldResource::collection(ICMCDWField::all());
+        $query = ICMCDWField::query();
+
+        if ($request->has('name')) {
+            $query->whereRaw('LOWER(name) LIKE ?', ['%' . strtolower($request->name) . '%']);
+        }
+
+        if ($request->has('field')) {
+            $query->whereRaw('LOWER(field) LIKE ?', ['%' . strtolower($request->field) . '%']);
+        }
+
+        if ($request->has('entity')) {
+            $query->whereRaw('LOWER(entity) LIKE ?', ['%' . strtolower($request->entity) . '%']);
+        }
+
+        if ($request->has('path')) {
+            $query->whereRaw('LOWER(path) LIKE ?', ['%' . strtolower($request->path) . '%']);
+        }
+
+        if ($request->has('bre_fields')) {
+            $query->whereHas('breFields', function ($query) use ($request) {
+                if ($request->filled('bre_fields')) {
+                    $input = $request->bre_fields;
+                    if (is_numeric($input)) {
+                        $query->where('bre_fields.id', $input);
+                    } else {
+                        $query->whereRaw('LOWER(name) LIKE ?', ['%' . strtolower($input) . '%']);
+                    }
+                }
+            });
+        }
+
+        if ($request->has('order_by')) {
+            $orderDirection = $request->input('order_direction', 'asc');
+            $query->orderBy($request->order_by, $orderDirection);
+        }
+
+        if ($request->has('limit')) {
+            $query->limit($request->limit);
+        }
+
+        if ($request->has('offset')) {
+            $query->offset($request->offset);
+        }
+
+        if ($request->has('search')) {
+            $searchTerm = strtolower($request->search);
+
+            $query->where(function ($query) use ($searchTerm) {
+                $query->whereRaw('LOWER(name) LIKE ?', ['%' . $searchTerm . '%'])
+                    ->orWhereRaw('LOWER(field) LIKE ?', ['%' . $searchTerm . '%'])
+                    ->orWhereRaw('LOWER(path) LIKE ?', ['%' . $searchTerm . '%'])
+                    ->orWhereRaw('LOWER(subject_area) LIKE ?', ['%' . $searchTerm . '%'])
+                    ->orWhereRaw('LOWER(applet) LIKE ?', ['%' . $searchTerm . '%'])
+                    ->orWhereRaw('LOWER(datatype) LIKE ?', ['%' . $searchTerm . '%'])
+                    ->orWhereRaw('LOWER(cdw_ui_caption) LIKE ?', ['%' . $searchTerm . '%'])
+                    ->orWhereRaw('LOWER(cdw_table_name) LIKE ?', ['%' . $searchTerm . '%'])
+                    ->orWhereRaw('LOWER(cdw_column_name) LIKE ?', ['%' . $searchTerm . '%']);
+            });
+        }
+
+        $icmcdwFields = $query->get();
+        return ICMCDWFieldResource::collection($icmcdwFields);
     }
 
     public function show($id)
