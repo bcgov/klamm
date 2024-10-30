3db7825d32c2fc4cc8832d27692ec2bdce46efb4
Will Kiiskila <will.kiiskila@gov.bc.ca>
2024-10-30 12:12:25 -0700

    Add form fields to View mode

--- app/Filament/Forms/Resources/FormVersionResource/Pages/ViewFormVersion.php
+++ app/Filament/Forms/Resources/FormVersionResource/Pages/ViewFormVersion.php
@@ -16,4 +16,85 @@ class ViewFormVersion extends ViewRecord
             Actions\EditAction::make(),
         ];
     }
+
+    protected function mutateFormDataBeforeFill(array $data): array
+    {
+        $data = array_merge($this->record->toArray(), $data);
+
+        $components = [];
+
+        $formFields = $this->record->formInstanceFields()
+            ->whereNull('field_group_instance_id')
+            ->get();
+
+        foreach ($formFields as $field) {
+            $validations = [];
+            foreach ($field->validations as $validation) {
+                $validations[] = [
+                    'type' => $validation->type,
+                    'value' => $validation->value,
+                    'error_message' => $validation->error_message,
+                ];
+            }
+
+            $components[] = [
+                'component_type' => 'form_field',
+                'form_field_id' => $field->form_field_id,
+                'label' => $field->label,
+                'data_binding' => $field->data_binding,
+                'conditional_logic' => $field->conditional_logic,
+                'styles' => $field->styles,
+                'validations' => $validations,
+                'order' => $field->order,
+            ];
+        }
+
+        $fieldGroups = $this->record->fieldGroupInstances()->get();
+
+        foreach ($fieldGroups as $group) {
+            $groupFields = $group->formInstanceFields()->orderBy('order')->get();
+
+            $formFieldsData = [];
+            foreach ($groupFields as $field) {
+                $validations = [];
+                foreach ($field->validations as $validation) {
+                    $validations[] = [
+                        'type' => $validation->type,
+                        'value' => $validation->value,
+                        'error_message' => $validation->error_message,
+                    ];
+                }
+
+                $formFieldsData[] = [
+                    'form_field_id' => $field->form_field_id,
+                    'label' => $field->label,
+                    'data_binding' => $field->data_binding,
+                    'conditional_logic' => $field->conditional_logic,
+                    'styles' => $field->styles,
+                    'validations' => $validations,
+                ];
+            }
+
+            $components[] = [
+                'component_type' => 'field_group',
+                'field_group_id' => $group->field_group_id,
+                'group_label' => $group->label,
+                'repeater' => $group->repeater,
+                'form_fields' => $formFieldsData,
+                'order' => $group->order,
+            ];
+        }
+
+        usort($components, function ($a, $b) {
+            return $a['order'] <=> $b['order'];
+        });
+
+        foreach ($components as &$component) {
+            unset($component['order']);
+        }
+
+        $data['components'] = $components;
+
+        return $data;
+    }
 }
