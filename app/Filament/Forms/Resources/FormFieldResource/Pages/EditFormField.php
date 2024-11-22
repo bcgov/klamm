a42acb4d0f8e3ecde98ba34ef6655fac59ff9c8c
Will Kiiskila <will.kiiskila@gov.bc.ca>
2024-11-22 11:11:38 -0800

    prevent form field deletion if used in form instance

--- app/Filament/Forms/Resources/FormFieldResource/Pages/EditFormField.php
+++ app/Filament/Forms/Resources/FormFieldResource/Pages/EditFormField.php
@@ -3,8 +3,10 @@
 namespace App\Filament\Forms\Resources\FormFieldResource\Pages;
 
 use App\Filament\Forms\Resources\FormFieldResource;
-use Filament\Actions;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\ViewAction;
 use Filament\Resources\Pages\EditRecord;
+use Filament\Notifications\Notification;
 
 class EditFormField extends EditRecord
 {
@@ -13,8 +15,19 @@ class EditFormField extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\ViewAction::make(),
-            Actions\DeleteAction::make(),
+            ViewAction::make(),
+            DeleteAction::make()
+                ->before(function (DeleteAction $action) {
+                    if ($this->record->formInstanceFields()->exists()) {
+                        Notification::make()
+                            ->danger()
+                            ->title('Cannot Delete Form Field')
+                            ->body('This form field is in use by one or more form instances and cannot be deleted.')
+                            ->send();
+
+                        $action->halt();
+                    }
+                }),
         ];
     }
 }
