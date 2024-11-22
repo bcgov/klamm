474d286f685aed22482813f859984a68c6a9d73a
Will Kiiskila <will.kiiskila@gov.bc.ca>
2024-11-22 12:01:55 -0800

    form fields list form versions when trying to delete

--- app/Filament/Forms/Resources/FormFieldResource/Pages/EditFormField.php
+++ app/Filament/Forms/Resources/FormFieldResource/Pages/EditFormField.php
@@ -18,11 +18,24 @@ class EditFormField extends EditRecord
             ViewAction::make(),
             DeleteAction::make()
                 ->before(function (DeleteAction $action) {
-                    if ($this->record->formInstanceFields()->exists()) {
+                    if ($this->record->formVersions()->exists()) {
+
+                        $formVersions = $this->record->formVersions()->with('form')->get();
+
+                        $versionsList = $formVersions->map(function ($version) {
+                            $formName = $version->form->form_title ? $version->form->form_id : 'Unknown Form';
+                            $trimmedFormName = strlen($formName) > 20
+                                ? substr($formName, 0, 17) . '...'
+                                : $formName;
+                            $versionNumber = $version->version_number ?? 'Unknown Version';
+                            return 'Form: ' . $trimmedFormName . ', V: ' . $versionNumber . '<br />';
+                        })->implode("");
+
                         Notification::make()
                             ->danger()
                             ->title('Cannot Delete Form Field')
-                            ->body('This form field is in use by one or more form instances and cannot be deleted.')
+                            ->body('This form field is in use by the following form versions and cannot be deleted:' . "<br /></br />" . $versionsList)
+                            ->persistent()
                             ->send();
 
                         $action->halt();
