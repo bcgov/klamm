29e431ef7e44354dee8cba4508db7a41f64f8c54
Will Kiiskila <will.kiiskila@gov.bc.ca>
2025-01-10 09:52:34 -0800

    add error tables for system messages

---
+++ app/Filament/Fodig/Resources/ErrorSourceResource.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Filament\Fodig\Resources;
+
+use App\Filament\Fodig\Resources\ErrorSourceResource\Pages;
+use App\Filament\Fodig\Resources\ErrorSourceResource\RelationManagers;
+use App\Models\ErrorSource;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ErrorSourceResource extends Resource
+{
+    protected static ?string $model = ErrorSource::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-home';
+
+    protected static ?string $navigationGroup = 'Error Lookup Tool';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Textarea::make('name')
+                    ->required()
+                    ->columnSpanFull(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                //
+            ])
+            ->paginated([
+                10,
+                25,
+                50,
+                100,
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListErrorSources::route('/'),
+            'create' => Pages\CreateErrorSource::route('/create'),
+            'view' => Pages\ViewErrorSource::route('/{record}'),
+            'edit' => Pages\EditErrorSource::route('/{record}/edit'),
+        ];
+    }
+}
