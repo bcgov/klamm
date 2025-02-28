944c4dc107e34157ee19801b386830628eafede6
brysonjbest <103070659+brysonjbest@users.noreply.github.com>
2025-02-28 15:01:22 -0800

    Init Siebel Fields.

---
+++ database/seeders/SiebelFieldsTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class SiebelFieldsTableSeeder extends Seeder
+{
+
+    /**
+     * Auto generated seed file
+     *
+     * @return void
+     */
+    public function run()
+    {
+        ini_set('memory_limit', '1024M');
+
+        \DB::table('siebel_fields')->delete();
+
+        $this->call([
+            SiebelFieldsTableSeeder1::class,
+            SiebelFieldsTableSeeder2::class,
+            SiebelFieldsTableSeeder3::class,
+            SiebelFieldsTableSeeder4::class,
+            SiebelFieldsTableSeeder5::class,
+            SiebelFieldsTableSeeder6::class,
+            SiebelFieldsTableSeeder7::class,
+            SiebelFieldsTableSeeder8::class,
+            SiebelFieldsTableSeeder9::class,
+            SiebelFieldsTableSeeder10::class,
+        ]);
+    }
+}
