7725439a824da6da1cd6a1019adedaf5e0758832
Will Kiiskila <will.kiiskila@gov.bc.ca>
2025-03-12 07:55:16 -0700

    remove rendered form and preview button

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FormViewController;
 
-// Uses the react deployment to render a form based on UUID
-Route::get('/forms/rendered-forms/{uuid}/view', [FormViewController::class, 'show'])->name('forms.rendered_forms.view');
-
-// For rendering our catalogued forms
-Route::get('/forms/rendered-forms/preview', [FormViewController::class, 'preview'])->name('forms.rendered_forms.preview');
-
 // Explicit login route (otherwise it only lives in the admin panel)
 Route::get('/login', function () {
     return redirect(route('filament.admin.auth.login'));
