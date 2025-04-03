4febb26594bd59ba8d20df412177b6f875fc8a03
Will Kiiskila <will.kiiskila@gov.bc.ca>
2025-04-03 12:13:33 -0700

    add description to report entries

--- app/Filament/Reports/Resources/ReportEntryResource/Pages/ListReportEntries.php
+++ app/Filament/Reports/Resources/ReportEntryResource/Pages/ListReportEntries.php
@@ -5,13 +5,15 @@ namespace App\Filament\Reports\Resources\ReportEntryResource\Pages;
 use App\Filament\Reports\Resources\ReportEntryResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
-use Filament\Support\Colors\Color;
 
 class ListReportEntries extends ListRecords
 {
     protected static string $resource = ReportEntryResource::class;
 
     protected static ?string $title = 'Report Label Dictionary';
+
+    protected ?string $subheading = 'A dictionary of all report labels, designed to standardize labels and streamline future report requirements for financial components.';
+
 
     protected function getHeaderActions(): array
     {
@@ -24,6 +26,8 @@ class ListReportEntries extends ListRecords
 
     public function getBreadcrumbs(): array
     {
-        return [];
+        return [
+            'https://knowledge.social.gov.bc.ca/successor/financial_components/product-team/agreements-backlog/problem-solving/report-dictionary' => 'Link to our Problem Statement',
+        ];
     }
 }
