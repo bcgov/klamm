c0510955302638d683043a4c04b831bf20513fa2
Will Kiiskila <will.kiiskila@gov.bc.ca>
2025-06-17 09:21:24 -0700

    remove form version navigation

--- app/Filament/Forms/Resources/FormVersionResource.php
+++ app/Filament/Forms/Resources/FormVersionResource.php
@@ -29,7 +29,7 @@ class FormVersionResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-inbox-stack';
 
-    protected static bool $shouldRegisterNavigation = true;
+    protected static bool $shouldRegisterNavigation = false;
 
     // Function to get the current counter value from the session
     public static function getElementCounter(): int
