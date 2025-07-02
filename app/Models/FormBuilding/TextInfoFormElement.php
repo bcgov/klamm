dc35620d16ae20008eb787d6f11399034d24c0df
Joh Yoshida <johtaro.yoshida@gov.bc.ca>
2025-07-02 14:18:24 -0700

    Use Markdown editor for Textinfo content

--- app/Models/FormBuilding/TextInfoFormElement.php
+++ app/Models/FormBuilding/TextInfoFormElement.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\FormBuilding;
 
+use Filament\Forms\Components\MarkdownEditor;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
@@ -20,9 +21,8 @@ class TextInfoFormElement extends Model
     public static function getFilamentSchema(bool $disabled = false): array
     {
         return [
-            \Filament\Forms\Components\Textarea::make('elementable_data.content')
+            MarkdownEditor::make('elementable_data.content')
                 ->label('Content')
-                ->rows(4)
                 ->disabled($disabled),
         ];
     }
