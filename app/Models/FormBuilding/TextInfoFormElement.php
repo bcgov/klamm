1be294183aeae941b5d31f564e9198f20bddaca1
Joh Yoshida <johtaro.yoshida@gov.bc.ca>
2025-07-03 09:58:19 -0700

    Revert "Use Markdown editor for Textinfo content"
    This reverts commit dc35620d16ae20008eb787d6f11399034d24c0df.

--- app/Models/FormBuilding/TextInfoFormElement.php
+++ app/Models/FormBuilding/TextInfoFormElement.php
@@ -2,7 +2,6 @@
 
 namespace App\Models\FormBuilding;
 
-use Filament\Forms\Components\MarkdownEditor;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
@@ -21,8 +20,9 @@ class TextInfoFormElement extends Model
     public static function getFilamentSchema(bool $disabled = false): array
     {
         return [
-            MarkdownEditor::make('elementable_data.content')
+            \Filament\Forms\Components\Textarea::make('elementable_data.content')
                 ->label('Content')
+                ->rows(4)
                 ->disabled($disabled),
         ];
     }
