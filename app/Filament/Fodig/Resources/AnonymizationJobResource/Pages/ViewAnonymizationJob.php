e5ae85f242436973299fc2a302d8f620a3439112
brysonjbest <103070659+brysonjbest@users.noreply.github.com>
2026-03-10 15:53:39 -0700

    Add job duplication for easy recreation.

--- app/Filament/Fodig/Resources/AnonymizationJobResource/Pages/ViewAnonymizationJob.php
+++ app/Filament/Fodig/Resources/AnonymizationJobResource/Pages/ViewAnonymizationJob.php
@@ -56,6 +56,19 @@ class ViewAnonymizationJob extends ViewRecord
                         ->body('The anonymization script will refresh automatically once complete.')
                         ->send();
                 }),
+            Actions\Action::make('duplicate')
+                ->label('Duplicate')
+                ->icon('heroicon-o-document-duplicate')
+                ->color('info')
+                ->requiresConfirmation()
+                ->modalHeading('Duplicate anonymization job?')
+                ->modalDescription('This creates a new draft job with the same settings, scope, and column selections. The SQL script is regenerated for the new copy.')
+                ->modalSubmitActionLabel('Duplicate job')
+                ->action(function () {
+                    $duplicate = AnonymizationJobResource::duplicateJob($this->record);
+
+                    return $this->redirect(AnonymizationJobResource::getUrl('edit', ['record' => $duplicate]));
+                }),
             Actions\Action::make('viewSelection')
                 ->label('View Selection')
                 ->icon('heroicon-o-list-bullet')
