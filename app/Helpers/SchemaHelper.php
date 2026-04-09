5c7b33a092a57d7fc579d06b85294a2a749007db
JohYoshidaBCGov <johtaro.yoshida@gov.bc.ca>
2026-04-09 13:37:20 -0700

    De-duplicate options repeaters

--- app/Helpers/SchemaHelper.php
+++ app/Helpers/SchemaHelper.php
@@ -5,6 +5,8 @@ namespace App\Helpers;
 use Filament\Forms\Components\Fieldset;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Actions\Action;
 
 class SchemaHelper
@@ -76,5 +78,75 @@ class SchemaHelper
                 Make sure you also insert the Moustache library in the Scripts tab and register the variable.')
             ->default(false)
             ->disabled($disabled);
+    }
+
+    public static function getOptionsRepeater(bool $disabled = false, string $label = 'Options'): Repeater
+    {
+        return Repeater::make('elementable_data.options')
+            ->label($label)
+            ->schema([
+                TextInput::make('label')
+                    ->label('Option Label')
+                    ->required()
+                    ->maxLength(255)
+                    ->columnSpan(2)
+                    ->autocomplete(false)
+                    ->live(onBlur: true)
+                    ->afterStateUpdated(function (callable $set, callable $get, $state) {
+                        $value = $get('value');
+                        if (empty($value) && !empty($state)) {
+                            $slug = \Illuminate\Support\Str::slug($state, '-');
+                            $set('value', $slug);
+                        }
+                    }),
+                TextInput::make('value')
+                    ->label('Option Value')
+                    ->required()
+                    ->maxLength(255)
+                    ->columnSpan(2)
+                    ->suffixAction(
+                        Action::make('regenerate_value')
+                            ->icon('heroicon-o-arrow-path')
+                            ->tooltip('Regenerate from Option Label')
+                            ->action(function (callable $set, callable $get) {
+                                $label = $get('label');
+                                if (!empty($label)) {
+                                    $slug = \Illuminate\Support\Str::slug($label, '-');
+                                    $set('value', $slug);
+                                }
+                            })
+                    ),
+            ])
+            ->columns(2)
+            ->defaultItems(1)
+            ->addActionLabel('Add Option')
+            ->reorderableWithButtons()
+            ->collapsible()
+            ->itemLabel(fn(array $state): ?string => $state['label'] ?? 'Option')
+            ->disabled($disabled)
+            ->minItems(1);
+    }
+
+    public static function getOptionsDefaultSelectedSelect(bool $disabled = false, bool $multiple = false, string $label = 'Default Selected Value'): Select
+    {
+        $select = Select::make('elementable_data.defaultSelected')
+            ->label($label)
+            ->options(function (callable $get) {
+                $options = $get('elementable_data.options') ?? [];
+                $selectOptions = [];
+                foreach ($options as $option) {
+                    if (!empty($option['value'])) {
+                        $selectOptions[$option['value']] = $option['label'] ?? $option['value'];
+                    }
+                }
+                return $selectOptions;
+            })
+            ->disabled($disabled);
+
+        if ($multiple) {
+            $select->multiple()->nullable()->live();
+        }
+
+        return $select;
     }
 }
